faf9d5bfd3c9bde1dabad07eeaabfeb56a8e28ca
Kajigga Dev <kajigga@gmail.com>
2018-06-04 15:20:20 -0600

    Make url a list

--- _beacons/rss.py
+++ _beacons/rss.py
@@ -45,27 +45,33 @@ def validate(_config):
 def beacon(config):
     '''Check the RSS feed for changes'''
     list_of_dicts = []
-    for _url in config.get('url'):
-      parsed_feed = feedparser.parse(_url)
-      # Generate MD5 hash of the most current item's title and link elements.
-      lasthash = __grains__.get('last_rss_hash_{}'.format(_url))
-      log.debug('{} parsed with {} entries'.format(
-                config.get('url'), len(parsed_feed)))
-      current_lasthash = hashlib.md5(parsed_feed.entries[0].link +
-                                     parsed_feed.entries[0].title).hexdigest()
-      log.debug('feed modified on %s', parsed_feed.modified)
-      # Thu, 06 Mar 2014 00:13:50 GMT
-      log.debug('current lasthash %s', current_lasthash)
-      # 4167402f1ba2629fcc71003121aa1d25
+    url_list = config.get('url', [])
+    if type(url_list) != list:
+        url_list = []
+        url_list.append(config.get('url', []))
 
-      if lasthash != current_lasthash:
-          # This rss feed has changed
-          __salt__['grains.setval']('last_rss_hash', current_lasthash)
-          _event = {
-              'tag': 'my/beacons/rss/newentry',
-              'entry': parsed_feed.entries[0]['link']
-              }
-          list_of_dicts.append(_event)
-          log.debug('sending beacon event because there is a new rss entry')
+    for _url in url_list:
+        parsed_feed = feedparser.parse(_url)
+        # Generate MD5 hash of the most current item's title and link elements.
+        lasthash = __grains__.get('last_rss_hash_{}'.format(_url))
+        log.debug('{} parsed with {} entries'.format(
+                  config.get('url'), len(parsed_feed)))
+        current_lasthash = hashlib.md5(parsed_feed.entries[0].link +
+                                       parsed_feed.entries[0].title
+                                       ).hexdigest()
+        log.debug('feed modified on %s', parsed_feed.modified)
+        # Thu, 06 Mar 2014 00:13:50 GMT
+        log.debug('current lasthash %s', current_lasthash)
+        # 4167402f1ba2629fcc71003121aa1d25
+
+        if lasthash != current_lasthash:
+            # This rss feed has changed
+            __salt__['grains.setval']('last_rss_hash', current_lasthash)
+            _event = {
+                'tag': 'my/beacons/rss/newentry',
+                'entry': parsed_feed.entries[0]['link']
+                }
+            list_of_dicts.append(_event)
+            log.debug('sending beacon event because there is a new rss entry')
 
     return list_of_dicts
