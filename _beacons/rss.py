68329ba411018df5ec80a903e9d7e67609d8c6d6
Kajigga Dev <kajigga@gmail.com>
2018-06-04 15:13:17 -0600

    Make the `url` config field be a list, allowing for multiple rss feeds to be parsed.

--- _beacons/rss.py
+++ _beacons/rss.py
@@ -22,31 +22,50 @@ def __virtual__():
     return False
 
 
+def validate(_config):
+    print('config', _config, type(_config))
+    valid_config = True
+    comments = []
+    # _config = {}
+    # list(map(_config.update, config))
+
+    if 'url' not in _config:
+        comments.append('configuration for rss beacons must have a key "url"')
+        valid_config = False
+
+    for _url in _config.get('url'):
+        if _url[:4] != 'http':
+            comments.append(
+                    '''configuration "url" must start with "http"
+                    but it is {}'''.format(_url))
+            valid_config = False
+    return valid_config, comments
+
+
 def beacon(config):
     '''Check the RSS feed for changes'''
     list_of_dicts = []
-    parsed_feed = feedparser.parse(config.get('url'))
-    # Generate MD5 hash of the most current item's title and link elements.
-    lasthash = __grains__.get('last_rss_hash')
-    log.debug('{} parsed with {} entries'.format(
-              config.get('url'), len(parsed_feed)))
-    current_lasthash = hashlib.md5(parsed_feed.entries[0].link +
-                                   parsed_feed.entries[0].title).hexdigest()
+    for _url in config.get('url'):
+      parsed_feed = feedparser.parse(_url)
+      # Generate MD5 hash of the most current item's title and link elements.
+      lasthash = __grains__.get('last_rss_hash_{}'.format(_url))
+      log.debug('{} parsed with {} entries'.format(
+                config.get('url'), len(parsed_feed)))
+      current_lasthash = hashlib.md5(parsed_feed.entries[0].link +
+                                     parsed_feed.entries[0].title).hexdigest()
+      log.debug('feed modified on %s', parsed_feed.modified)
+      # Thu, 06 Mar 2014 00:13:50 GMT
+      log.debug('current lasthash %s', current_lasthash)
+      # 4167402f1ba2629fcc71003121aa1d25
 
-
-    log.debug('feed modified on %s', parsed_feed.modified)
-    # Thu, 06 Mar 2014 00:13:50 GMT
-    log.debug('current lasthash %s', current_lasthash)
-    # 4167402f1ba2629fcc71003121aa1d25
-
-    if lasthash != current_lasthash:
-        # This rss feed has changed
-        __salt__['grains.setval']('last_rss_hash', current_lasthash)
-        _event = {
-            'tag': 'my/beacons/rss/newentry',
-            'entry': parsed_feed.entries[0]['link']
-            }
-        list_of_dicts.append(_event)
-        log.debug('sending beacon event because there is a new rss entry')
+      if lasthash != current_lasthash:
+          # This rss feed has changed
+          __salt__['grains.setval']('last_rss_hash', current_lasthash)
+          _event = {
+              'tag': 'my/beacons/rss/newentry',
+              'entry': parsed_feed.entries[0]['link']
+              }
+          list_of_dicts.append(_event)
+          log.debug('sending beacon event because there is a new rss entry')
 
     return list_of_dicts
--- enable_beacon/init.sls
+++ enable_beacon/init.sls
@@ -1,16 +1,20 @@
-enable_rss_beacon:
-  beacon.present:
-    - save: True
-    - name: rss
-    - interval: 5
-    - url: http://rss.cnn.com/rss/cnn_topstories.rss
+install_python_feedparser:
+  pkg.installed:
+    - name: python-feedparser
 
-add_beacon_if_not_there:
-  module.run:
-    - beacons.add:
-      name: rss
-      beacon_data:
-        interval: 5
-        url: http://rss.cnn.com/rss/cnn_topstories.rss
-    - onfail:
-      - enable_rss_beacon
+# enable_rss_beacon:
+#   beacon.present:
+#     - save: True
+#     - name: rss
+#     - interval: 5
+#     - url: http://rss.cnn.com/rss/cnn_topstories.rss
+
+# add_beacon_if_not_there:
+#   module.run:
+#     - beacons.add:
+#       - name: rss
+#       beacon_data:
+#         interval: 5
+#         url: http://rss.cnn.com/rss/cnn_topstories.rss
+#     - onfail:
+#       - enable_rss_beacon
